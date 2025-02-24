8fba9e942004098eda27e499b3b0b1019cf9f60d
jyoti1234-rgb <jyotilohkare4@gmail.com>
2025-02-24 20:04:24 +0530

    Day_6

---
+++ Day6_Assighment1.py
@@ -0,0 +1,17 @@
+# Assignment 1 - Write a python program to reverse a number using a while loop.
+
+#taking input from user
+num = int(input("Enter the number : "))
+reverse_num = 0             # initializing reverse number
+
+while num > 0:              #initializing while loop
+    digit = num % 10
+    reverse_num = (reverse_num * 10) + digit        # Adding digit to reverse number
+    num = num // 10
+
+print("the reserve of the number is - " ,reverse_num)
+
+'''Output'''
+
+'''Enter the number : 1206
+the reserve of the number is -  6021'''
